66257f7393863dbc707da571afe13da84eceefac
Nhu Quynh Nguyen <nngu0106@student.monash.edu>
2022-04-29 10:14:07 +1000

    task 4

--- Assessment 2/battle.py
+++ Assessment 2/battle.py
@@ -1,5 +1,5 @@
 from poke_team import PokeTeam
-
+from pokemon_base import attack_calculation 
 
 class Battle():
     def rotating_mode_battle(self) -> str:
@@ -15,17 +15,18 @@ class Battle():
         pokemon_1 = team_1.front
         pokemon_2 = team_2.front 
 
+
+
         while team_1.is_empty() == False or team_2.is_empty() == False: 
             if pokemon_1.get_speed() > pokemon_2.get_speed():
-                pokemon_1.get_attack()
-                pokemon_2.get_defend() 
+                pokemon_1_attack = pokemon_1.attack_calculation()
+                pokemon_2.damage_after_attack(pokemon_1_attack)
 
                 if pokemon_2.hp <= 0: 
                     pokemon_2.serve()  
                     pokemon_1.update_level()
                     temp_1 = pokemon_1.serve() 
                     team_1.append(temp_1)
-
 
                 elif pokemon_2.hp > 0: 
                     pokemon_1.hp -= 1  
@@ -40,8 +41,8 @@ class Battle():
 
                         
             elif pokemon_1.get_speed() < pokemon_2.get_speed(): 
-                pokemon_2.get_attack()
-                pokemon_1.get_defend() 
+                pokemon_2_attack = pokemon_2.attack_calculation()
+                pokemon_1.damage_after_attack(pokemon_2_attack)
 
                 if pokemon_1.hp <= 0: 
                     pokemon_1.serve()  
@@ -61,10 +62,10 @@ class Battle():
 
 
             elif pokemon_1.get_speed() == pokemon_2.get_speed(): 
-                pokemon_1.get_attack()
-                pokemon_1.get_defend()  
-                pokemon_2.get_attack()
-                pokemon_2.get_defend() 
+                pokemon_1_attack = pokemon_1.attack_calculation()
+                pokemon_2.damage_after_attack(pokemon_1_attack)
+                pokemon_2_attack = pokemon_2.attack_calculation()
+                pokemon_1.damage_after_attack(pokemon_2_attack) 
                 pokemon_1.hp -= 1  
                 pokemon_2.hp -= 1 
                 temp_1 = pokemon_1.serve() 
