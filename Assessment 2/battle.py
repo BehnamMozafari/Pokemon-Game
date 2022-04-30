1194e0eeefcee3915cf1b13e372d17a0b9ca788f
Nhu Quynh Nguyen <nngu0106@student.monash.edu>
2022-04-30 15:51:15 +1000

    Testing battle class

--- Assessment 2/battle.py
+++ Assessment 2/battle.py
@@ -74,20 +74,17 @@ class Battle():
                 # Pokemon_1 is levelled up, that pokemon_1 get brought to the end of the queue
                 if pokemon_2.get_hp() <= 0:
                     pokemon_1.update_level()
-                    temp_1 = pokemon_1
-                    self.team_1.team.append(temp_1)
+                    self.team_1.team.append(pokemon_1)
 
                 # If pokemon_2 is alive after the attack, both pokemon's hp is decreased by 1. Whichever pokemon is
                 # alive after the decrease, get sent back to the end of the array or deleted if hp less than equal 0
                 elif pokemon_2.get_hp() > 0:
                     pokemon_1.decrease_hp()
                     pokemon_2.decrease_hp()
-                    temp_1 = pokemon_1
-                    temp_2 = pokemon_2
                     if pokemon_1.hp > 0:
-                        self.team_1.team.append(temp_1)
+                        self.team_1.team.append(pokemon_1)
                     if pokemon_2.hp > 0:
-                        self.team_2.team.append(temp_2)
+                        self.team_2.team.append(pokemon_2)
 
             # Second check condition whether speed of pokemon_2 is larger than speed of pokemon_1:
             elif pokemon_1.get_speed() < pokemon_2.get_speed():
@@ -98,20 +95,17 @@ class Battle():
                 # Pokemon_2 is levelled up, that pokemon_2 get brought to the end of the queue
                 if pokemon_1.get_hp <= 0:
                     pokemon_2.update_level()
-                    temp_2 = pokemon_2
-                    self.team_2.team.append(temp_2)
+                    self.team_2.team.append(pokemon_2)
 
                 # If pokemon_1 is alive after the attack, both pokemon's hp is decreased by 1. Whichever pokemon is
                 # alive after the decrease, get sent back to the end of the array or deleted if hp <= 0
                 elif pokemon_1.get_hp > 0:
                     pokemon_2.decrease_hp()
                     pokemon_1.decrease_hp()
-                    temp_2 = pokemon_2
-                    temp_1 = pokemon_1
                     if pokemon_2.hp > 0:
-                        self.team_2.team.append(temp_2)
+                        self.team_2.team.append(pokemon_2)
                     if pokemon_1.hp > 0:
-                        self.team_1.team.append(temp_1)
+                        self.team_1.team.append(pokemon_1)
 
             # Third check condition: if the speed of pokemon_1 and pokemon_2 equals, calculate the hp after attack
             # Each pokemon hp is decreased by 1 as well
@@ -122,12 +116,10 @@ class Battle():
                 pokemon_1.damage_after_attacked(pokemon_2)
                 pokemon_1.decrease_hp()
                 pokemon_2.decrease_hp()
-                temp_1 = pokemon_1
-                temp_2 = pokemon_2
                 if pokemon_1.hp > 0:
-                    self.team_1.team.append(temp_1)
+                    self.team_1.team.append(pokemon_1)
                 if pokemon_2.hp > 0:
-                    self.team_2.team.append(temp_2)
+                    self.team_2.team.append(pokemon_2)
 
         # If eventually both team is empty, print Draw to the console
         if self.team_1.team.is_empty() == True and self.team_2.team.is_empty() == True:
@@ -139,3 +131,7 @@ class Battle():
             # If team_2 has no pokemon left, print team_1 wins to the console
             elif self.team_2.team.is_empty():
                 return self.team_1.team_name
+
+if __name__ == '__main__':
+    x  = Battle("Nhu","Khoa");
+    print(x.rotating_mode_battle())
