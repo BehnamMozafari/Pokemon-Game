d18f13476e8e16176b577d67374d5cf4c1ee9b44
Zhongxun Pan <zpan0008@student.monash.edu>
2022-05-01 15:21:08 +1000

    updating set_mode_battle method to project requirement standard.

--- Assessment 2/battle.py
+++ Assessment 2/battle.py
@@ -9,37 +9,104 @@ class Battle():
         self.battle_mode = None
 
     def set_mode_battle(self) -> str:
+        """
+        this method which asks the user for input and sets up the
+        players’ teams in such an order where a pokemon fights until it faints. It is here where
+        you set the team’s battle mode to 0. This method should not take any arguments.
+        This method returns the name of the player that wins the battle, Draw otherwise
+
+        NOTE: The order of the Pokemon in this battle mode MUST be Charmanders ->
+        Bulbasaurs -> Squirtles
+
+
+        An example of a set mode battle would be the following:
+        Team 1: C1 C2 B1 B2 S1
+        Team 2: B1 B2 S1
+        Round 1: Team 1’s C1 faints Team 2’s B1
+        Round 2: Team 1’s C1 faints Team 2’s B2
+        Round 3: Team 1’s C1 is fainted by Team 2’s S1
+        Round 4: Team 1’s C2 is fainted by Team 2’s S1
+        Round 5: Team 1’s B1 faints Team 2’s S1
+        """
+        # set battle mode to 0
         self.battle_mode = 0
         self.team_1.choose_team(self.battle_mode, None)
         self.team_2.choose_team(self.battle_mode, None)
 
         while self.team_1.team.__len__() >= 1 and self.team_2.team.__len__() >= 1:
+            # set u1 as the first pokemon to fight from stack for team 1, same goes for team 2 with u2
             u1 = self.team_1.team.pop()
             u2 = self.team_2.team.pop()
-            if u1.get_speed() > u2.get_speed():
+            # if they have the same speed, attack at the same time
+            if u1.get_speed() == u2.get_speed():
+                # both defend
+                u1.damage_after_attacked(u2)
                 u2.damage_after_attacked(u1)
-                if u2.get_hp() > 0:
-                    u1.damage_after_attacked(u2)
-                    if u1.get_hp() > 0:
+                if u1.get_hp() > 0 and u2.get_hp() > 0:
+                    # lose 1 hp
+                    u1.decrease_hp()
+                    u2.decrease_hp()
+                    if u1.get_hp() > 0 and u2.get_hp() > 0:
+                        # if both survive, push them back to stack
                         self.team_1.team.push(u1)
                         self.team_2.team.push(u2)
+                    elif u1.get_hp() > 0:
+                        # if u1 survive, level up and push to stack
+                        u1.update_level()
+                        self.team_1.team.push(u1)
+                    elif u2.get_hp() > 0:
+                        # if u2 survive, level up and push to stack
+                        u2.update_level()
+                        self.team_2.team.push(u2)
+                elif u1.get_hp() > 0:
+                    # if u1 survive, level up and push to stack
+                    u1.update_level()
+                    self.team_1.team.push(u1)
+                elif u2.get_hp() > 0:
+                    # if u2 survive, level up and push to stack
+                    u2.update_level()
+                    self.team_2.team.push(u2)
+
+            # if u1 has faster speed, u2 defend first
+            if u1.get_speed() > u2.get_speed():
+                u2.damage_after_attacked(u1)
+                # if u2 is still alive, u2 attacks and then u1 defends
+                if u2.get_hp() > 0:
+                    u1.damage_after_attacked(u2)
+                    # if both pokemon is alive, push them back to the array
+                    if u1.get_hp() > 0:
+                        u1.decrease_hp()
+                        u2.decrease_hp()
+                        self.team_1.team.push(u1)
+                        self.team_2.team.push(u2)
+                    # if u1 die after get attacked back u2, u2 levels up and add to the stack
                     else:
                         u2.update_level()
                         self.team_2.team.push(u2)
+                # if u2 die after get attacked back u1, u1 levels up and add to the stack
                 else:
                     u1.update_level()
                     self.team_1.team.push(u1)
-
+            # else u2 attack u1 beacuse of faster speed
             else:
+                # u1 defends attack from u2
                 u1.damage_after_attacked(u2)
+                # if u1 still alive, attacks u2
                 if u1.get_hp() > 0:
+                    # u2 defends u1 attack
                     u2.damage_after_attacked(u1)
+                    # if u2 and u1 both still alive
                     if u2.get_hp() > 0:
+                        # push them back to the stack
+                        u1.decrease_hp()
+                        u2.decrease_hp()
                         self.team_1.team.push(u1)
                         self.team_2.team.push(u2)
+                    # if u2 die after get attacked back u1, u1 levels up and add to the stack
                     else:
                         u1.update_level()
                         self.team_2.team.push(u1)
+                # if u1 die after get attacked back u2, u2 levels up and add to the stack
                 else:
                     u2.update_level()
                     self.team_2.team.push(u2)
